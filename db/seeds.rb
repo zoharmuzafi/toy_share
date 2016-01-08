99f8461652c36bc034949aeeac364695ae328f69
mrockway <mrockway@gmail.com>
2016-01-07 22:13:46 -0800

    toy controller complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+###### DELETE THIS LINE AND BELOW ##########
+toys = Toy.create([
+										{ name: "Legos", description: "Star Wars legos set", gender: "Male", age_range: "0-3", user_id: 1, city_id: 1 },
+										{ name: "Barbies", description: "Barbie doll"}, gender: "Female", age_range: "6-9", user_id: 2, city_id: 1 }
+									])
