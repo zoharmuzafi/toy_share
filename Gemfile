cc5bfe03660ab34d0dde11a78be470266f574d03
Cynthia Salinas <csalinas123@gmail.com>
2016-01-08 12:54:12 -0800

    add omniauth, create and destroy a session

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'ruby', '2.2.1'
+gem 'ruby'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use postgresql as the database for Active Record
@@ -51,3 +51,5 @@ group :development do
   gem 'spring'
 end
 
+# Omniauth for facebook 
+  gem 'omniauth-facebook'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	# Sends request to facebook
+  	auth = request.env["omniauth.auth"]
+  	# Requesting only basic information from facebook
+  	session[:omniauth] = auth.except('extra')
+  	# Allows user to sign in through omniauth 
+  	user = User.sign_in_from_omniauth(auth)
+
+    @user = User.find_by_email(params[:email]) 
+    if @user && @user.authenticate(params[:password]) 
+      # When signing in, users are assigned an user id
+      session[:user_id] = @user.id
+      # After signing in, users are directed to root path
+      redirect_to root_path, notice: "You are signed in!"
+    else 
+      flash[:error] = "Oops! Something went wrong."
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "You are logged out!"
+
   end
+  
 end
