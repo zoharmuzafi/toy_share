3b1980dd2738687b8ab031098f46fce377a90517
mrockway <mrockway@gmail.com>
2016-01-08 12:32:30 -0800

    save session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
   
   def new
-
   end
 
   def create
+  	@user = User.find_by_email(params[:email])
+  	if @user && @user.authenticate
 
+  	end
   end
 
   def destroy
 
   end
-  
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :password)
+  end
+
 end
