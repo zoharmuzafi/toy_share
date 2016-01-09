fbd0b3d2352375d2b7360397c8402708f980e055
Cynthia Salinas <csalinas123@gmail.com>
2016-01-09 12:22:56 -0800

    accidently deleted sessions page and inserted it again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # root 'welcome#index'
+  root 'welcome#index'
 
   get '/login', to: 'sessions#new', as: 'login'
   get '/signup', to: 'users#new', as: 'signup'
