dc12c1651df3d4c7461084b59c10cc82dd097472
DreTGiant <amassey30@yahoo.com>
2016-01-09 13:47:31 -0800

    fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -63,13 +63,16 @@ ActiveRecord::Schema.define(version: 20160109021817) do
     t.string   "l_name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "avatar"
     t.string   "bio"
     t.string   "provider"
     t.string   "uid"
     t.integer  "city_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
