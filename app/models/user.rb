e544078aba4a5679e5a9f75d1802940e23ce6d8b
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-11 15:16:55 -0800

    added D.V to the signup form and flash messages to layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,9 @@ class User < ActiveRecord::Base
 	has_many :toys, dependent: :destroy
 	belongs_to :city
 
+	validates :f_name, presence: true, length: { minimum: 2 }
+	validates :l_name, presence: true, length: { minimum: 2 }
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :password, presence: true, length: { minimum: 2 }
 end
 
