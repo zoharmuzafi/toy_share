41852fe5e77607eb4320771d387f8151f0624a7c
Cynthia Salinas <csalinas123@gmail.com>
2016-01-12 15:49:55 -0800

    accidently opened master schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109021817) do
+ActiveRecord::Schema.define(version: 20160112083729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20160109021817) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
