79ce471c0a6f7f8aeafced2981552c092c16e80c
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-12 15:43:07 -0800

    design login and signup modals

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,13 @@ class SessionsController < ApplicationController
 		if @user && @user.authenticate(params[:password])
 			flash[:notice] = "You have successfully logged in"
 			session[:user_id] = @user.id
-			redirect_to user_path(@user)
+      if params[:toy_show]
+        toy = Toy.find(params[:toy_id])
+        redirect_to toy_path(toy)
+      else
+			 redirect_to user_path(@user)
+      end
+
 		else
 			flash[:error] = "Username or password is incorrect"
 			redirect_to login_path
